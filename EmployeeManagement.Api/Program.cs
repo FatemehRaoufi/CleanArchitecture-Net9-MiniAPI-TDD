112964ae8c0fbf6f8c6a99bb36feb93505d30923
F R <raufi4u@gmail.com>
2024-12-07 01:12:41 +0100

    Added In-Memory Cashing

--- EmployeeManagement.Api/Program.cs
+++ EmployeeManagement.Api/Program.cs
@@ -2,10 +2,11 @@ using EmployeeManagement.Api.Models;
 using EmployeeManagement.Data;
 using EmployeeManagement.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory; // for using AddMemoryCache service
 
 //-----------------------------------------------------------
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddMemoryCache(); // Enable in-memory caching .net9
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("EmployeeDb"));// Using In-Memory database for simplicity
@@ -36,14 +37,42 @@ using (var scope = app.Services.CreateScope())
     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     AppDbContext.SeedDatabase(dbContext);
 }
-//------------------------------------------------------------------
-// Define API endpoints
-app.MapGet("/employees", async (IEmployeeService service) =>
+//-------------------------------------------------------------------
+/*
+How does it work?
+First Request: Data is fetched from the database and stored in the cache.
+Subsequent Requests: Data is served from the cache, reducing database load.
+
+De:
+wie funktioniert es?
+Erste Anfrage: Daten werden aus der Datenbank abgerufen und im Cache gespeichert.
+Folgende Anfragen: Daten werden aus dem Cache bereitgestellt, wodurch die Datenbanklast reduziert wird.
+ 
+ */
+// Define endpoints
+app.MapGet("/employees", async (IEmployeeService service, IMemoryCache cache) =>
+{
+    const string cacheKey = "all_employees";
+    if (!cache.TryGetValue(cacheKey, out List<Employee> employees))
+    {
+       var employee = await service.GetAllEmployeesAsync();
+
+        // Set cache options and add to cache
+        var cacheOptions = new MemoryCacheEntryOptions()
+            .SetSlidingExpiration(TimeSpan.FromMinutes(5)); // Cache expires after 5 minutes
+        cache.Set(cacheKey, employee, cacheOptions);
+    }
+    return Results.Ok(employees);
+});
+//............................................................
+// Define API endpoints (without AddMemoryCache service)
+/*app.MapGet("/employees", async (IEmployeeService service) =>
 {
     var employees = await service.GetAllEmployeesAsync();
     return Results.Ok(employees);
 });
-
+*/
+//...............................................................
 app.MapGet("/employees/{id:int}", async (IEmployeeService service, int id) =>
 {
     var employee = await service.GetEmployeeByIdAsync(id);
